2b4400edf6567fca3dd04c726cb4df89d6b1dd2f
DiogoSales <diogosales.ads@gmail.com>
2022-05-06 20:44:20 -0300

    requisito 17 ok

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const inputButton = document.querySelector('#button');
-inputButton.addEventListener('click', () => {
-  const inputEmail = document.querySelector('#inputEmail');
-  const inputSenha = document.querySelector('#inputSenha');
-  if (inputEmail.value !== 'tryber@teste.com' || inputSenha.value !== '123456') {
-    alert('Email ou senha inválidos.');
-  } else {
-    alert('Olá, Tryber!');
-  }
-});
+function login() {
+  const inputButton = document.querySelector('#button');
+  inputButton.addEventListener('click', () => {
+    const inputEmail = document.querySelector('#inputEmail');
+    const inputSenha = document.querySelector('#inputSenha');
+    if (inputEmail.value !== 'tryber@teste.com' || inputSenha.value !== '123456') {
+      alert('Email ou senha inválidos.');
+    } else {
+      alert('Olá, Tryber!');
+    }
+  });
+}
+login();
+
+/*  function sendButton() {
+  const btn = document.querySelector('#submit-btn');
+  btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+  });
+}
+sendButton();   */
